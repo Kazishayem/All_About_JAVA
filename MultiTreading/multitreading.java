172a6aa0cf289164f181ae25add2c9a5f5743bf0
Kazishayem <kazishayem1234@gmail.com>
2022-03-01 14:16:00 +0600

    first commit

--- MultiTreading/multitreading.java
+++ MultiTreading/multitreading.java
@@ -1,16 +1,22 @@
 package MultiTreading;
 
-public class multitreading {
+public class multitreading{
     public static void main(String[] args)  {
+
+        for(int i=0;i<5;i++){
+            theThings things1 = new theThings();
+            things1.start();
+
+        }
         
-        theThings things1 = new theThings();
-        theThings things2 = new theThings();
+        // theThings things1 = new theThings();
+        // theThings things2 = new theThings();
 
         // things1.run();
         // things2.run();
 
-        things1.start();
-        things2.start();
+        // things1.start();
+        // things2.start();
     }
 }
 
